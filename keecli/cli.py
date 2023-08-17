830c7ca16dff890da22caa8343953673e70245ee
Leo <leo.perard@ubisoft.com>
2023-08-17 11:43:38 -0400

    feat: command to only get the password from an entry :key:

--- keecli/cli.py
+++ keecli/cli.py
@@ -54,3 +54,29 @@ def get(ctx: click.Context, name: str, group: str, password: bool):
 
     click.echo(json.dumps([entry.to_dict(show_password=password) for entry in entries]))
     ctx.exit()
+
+
+@cli.command("pass")
+@click.pass_context
+@click.option(
+    "--group",
+    "-g",
+    type=click.STRING,
+    required=False,
+    default=None,
+    help="The group where to look for the entry.",
+)
+@click.argument("name", type=click.STRING)
+def password(ctx: click.Context, name: str, group: str):
+    """Get the password for an entry."""
+    db = ctx.obj["db"]
+    entries = db.get_entry(name, group=group)
+
+    if len(entries) > 1:
+        click.confirm("Multiple passwords found, do you want to continue?", abort=True)
+
+    if len(entries) == 0:
+        click.echo("No mathcing entry found")
+        ctx.exit(1)
+
+    click.echo(entries[0].password)
